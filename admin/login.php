ee35aed9f353aed2f0ea4a228e303c76ae9d8e94
jrotty <bssf@qq.com>
2022-06-16 20:09:36 +0800

    优化下input属性

--- admin/login.php
+++ admin/login.php
@@ -35,7 +35,7 @@ include('header.php');
 
             <div class="mt-4">
                 <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" for="LoggingEmailName">用户名</label>
-                <input id="LoggingEmailName" class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="text" name="username">
+                <input id="LoggingEmailName" class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="text" name="username" required>
             </div>
 
             <div class="mt-4">
@@ -43,7 +43,7 @@ include('header.php');
                     <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200" for="loggingPassword">密码</label>
                 </div>
 
-                <input id="loggingPassword" class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="password" name="pass">
+                <input id="loggingPassword" class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300" type="password" name="pass" required>
             </div>
 
             <div class="mt-8">
