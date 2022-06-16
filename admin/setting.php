1fd15e50c2674bc72c44a9d335c1d096ef596d46
jrotty <bssf@qq.com>
2022-06-16 20:16:14 +0800

    优化input属性

--- admin/setting.php
+++ admin/setting.php
@@ -25,7 +25,7 @@ require_once($path);
 <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
                 <div>
                     <label class="text-gray-700 dark:text-gray-200" for="username">网站地址</label>
-                    <input x-model="siteurl" name="siteurl" type="text" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
+                    <input x-model="siteurl" name="siteurl" type="url" class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring">
                 </div>
                 <div>
                     <label class="text-gray-700 dark:text-gray-200" for="username">网站名</label>
