19d94ec2cd57cc097619ecf8bebda54bc327a53f
jrotty <bssf@qq.com>
2022-06-19 01:00:07 +0800

    Update links.php

--- api/links.php
+++ api/links.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 include('../config/link.php');
-function newlinks($links){//新建分类时获取最新mid
+function newlinks($links){//将链接按照修改时间降序排列
 $Arrzong=$links;
 // 重新拍排序
 if(count($Arrzong)){//获取当前数组长度
@@ -22,24 +22,24 @@ if(count($Arrzong)){//获取当前数组长度
     
 return $Arrzong;
 }
-$newlinks=$links;
+$newlinks=$links;//没有任何参数时返回默认所有链接
 
-if(!empty($_GET['num'])){
+if(!empty($_GET['num'])){//如果有num参数，我们就返回对应最新的num条链接信息
 $newlinks=newlinks($links);
 $newlinks=array_slice($newlinks,0,$_GET['num']);   
 }
-if(!empty($_GET['new'])){
+
+if(!empty($_GET['new'])){//按照修改时间排序并返回所有
 $newlinks=newlinks($links);
 }
-if(!empty($_GET['mid'])){
-    
+
+if(!empty($_GET['mid'])){//通过分类mid获取链接(实际顺序)
 foreach ($links as $key => $value) {
 if($value['mid'] != $_GET['mid']){
 unset($links[$key]);
 }
 }
 $newlinks=$links;   
-    
 }
 
 echo json_encode($newlinks);
