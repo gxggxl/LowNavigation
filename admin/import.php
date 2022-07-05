4ecd5fef407d5e9d9e4b7125d54885725326e600
jrotty <bssf@qq.com>
2022-07-05 20:29:43 +0800

    尝试解决导入是乱码问题

--- admin/import.php
+++ admin/import.php
@@ -58,10 +58,10 @@ return true;
 function ReadCsv($uploadfile='')
     {
         setlocale(LC_ALL,'zh_CN');
- 
+        
         $file = fopen($uploadfile, "r");
         while (!feof($file)) {
-            $data[] = fgetcsv($file);
+            $data[] = fgetcsv_reg($file);
         }
         
         foreach ($data as $key => $value) {
@@ -74,15 +74,54 @@ function ReadCsv($uploadfile='')
  
         $head = $data[0];
         unset($data[0]);
- 
+        
         foreach ($data as $key => $value){
             foreach ($head as $k => $v){
                 $res[$key][$v] = $value[$k];
             }
         }
- 
         return $res;
-    }
+}
+    
+    
+function fgetcsv_reg(& $handle, $length = null, $d = ',', $e = '"') {
+$d = preg_quote($d);
+
+$e = preg_quote($e);
+
+$_line = "";
+
+$eof=false;
+
+while ($eof != true) {
+$_line .= (empty ($length) ? fgets($handle) : fgets($handle, $length));
+
+$itemcnt = preg_match_all('/' . $e . '/', $_line, $dummy);
+
+if ($itemcnt % 2 == 0)
+
+$eof = true;
+
+}
+
+$_csv_line = preg_replace('/(?: |[ ])?$/', $d, trim($_line));
+
+$_csv_pattern = '/(' . $e . '[^' . $e . ']*(?:' . $e . $e . '[^' . $e . ']*)*' . $e . '|[^' . $d . ']*)' . $d . '/';
+
+preg_match_all($_csv_pattern, $_csv_line, $_csv_matches);
+
+$_csv_data = $_csv_matches[1];
+
+for ($_csv_i = 0; $_csv_i < count($_csv_data); $_csv_i++) {
+$_csv_data[$_csv_i] = preg_replace('/^' . $e . '(.*)' . $e . '$/s', '$1', $_csv_data[$_csv_i]);
+
+$_csv_data[$_csv_i] = str_replace($e . $e, $e, $_csv_data[$_csv_i]);
+
+}
+
+return empty ($_line) ? false : $_csv_data;
+
+}
 ?>
 
 
@@ -97,11 +136,13 @@ function ReadCsv($uploadfile='')
 <!--导入-->
 <div class="bg-white dark:bg-black p-2 sm:p-6 mt-9"> 
 <h2 class="flex items-center text-xl font-medium text-gray-800 capitalize dark:text-white md:text-2xl"><svg class="w-6 h-6 inline text-blue-700 mr-0.5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>批量导入</h2>
-<p class="mt-2 text-gray-800 dark:text-gray-200">请使用wps将excel的前4列，列名分别设置为name，dis，site，mid，对应的列内容为网站名字，网站描述，网站地址，分类mid，其中网站描述列内容可以为空，然后导出csv，最后将csv在这里上传即可！【注意：批量导入不会检查重复项，建议导入前使用excel去重】</p>
+<p class="mt-2 text-gray-800 dark:text-gray-200">请使用wps将excel的前4列，列名分别设置为name，dis，site，mid，对应的列内容为网站名字，网站描述，网站地址，分类mid，其中网站描述列内容可以为空，然后导出csv(以逗号分割)文件，最后将csv在这里上传即可！</p>
 
 <div class="mt-4">
 <form action="<?php echo siteurl; ?>/admin/import.php?up" method="post" enctype="multipart/form-data">
 <input type="file" name="up"  accept="text/csv" class="w-full text-gray-700 px-3 py-2 border rounded">
+<p class="mt-2 text-gray-800 dark:text-gray-200">【注意：1,操作前请先去备份一下，以免导入故障造成数据异常。2，批量导入不会检查重复项，建议导入前使用excel去重】</p>
+
 <button type="submit" class="mt-4 px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">导入</button>
 </form>
 
