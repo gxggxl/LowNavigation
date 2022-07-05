fa33593bfc30a85e980d5dc4cb4c57015fe089e6
jrotty <bssf@qq.com>
2022-07-05 21:37:04 +0800

    解决部分用户批量导入导致的乱码问题

--- admin/import.php
+++ admin/import.php
@@ -58,70 +58,32 @@ return true;
 function ReadCsv($uploadfile='')
     {
         setlocale(LC_ALL,'zh_CN');
-        
         $file = fopen($uploadfile, "r");
         while (!feof($file)) {
-            $data[] = fgetcsv_reg($file);
+            $data[] = fgetcsv($file);
         }
         
-        foreach ($data as $key => $value) {
 
+        
+        foreach ($data as $key => $value) {
             if (!$value) {
                 unset($data[$key]);
             }
         }
         fclose($file);
- 
         $head = $data[0];
         unset($data[0]);
-        
+ 
         foreach ($data as $key => $value){
             foreach ($head as $k => $v){
+             $encoding = array('UTF-8', 'ASCII', 'GB2312', 'GBK');
+             $value[$k] = mb_convert_encoding($value[$k], "UTF-8", mb_detect_encoding($value[$k],$encoding));//将编码转为UTF-8
                 $res[$key][$v] = $value[$k];
             }
         }
+ 
         return $res;
-}
-    
-    
-function fgetcsv_reg(& $handle, $length = null, $d = ',', $e = '"') {
-$d = preg_quote($d);
-
-$e = preg_quote($e);
-
-$_line = "";
-
-$eof=false;
-
-while ($eof != true) {
-$_line .= (empty ($length) ? fgets($handle) : fgets($handle, $length));
-
-$itemcnt = preg_match_all('/' . $e . '/', $_line, $dummy);
-
-if ($itemcnt % 2 == 0)
-
-$eof = true;
-
-}
-
-$_csv_line = preg_replace('/(?: |[ ])?$/', $d, trim($_line));
-
-$_csv_pattern = '/(' . $e . '[^' . $e . ']*(?:' . $e . $e . '[^' . $e . ']*)*' . $e . '|[^' . $d . ']*)' . $d . '/';
-
-preg_match_all($_csv_pattern, $_csv_line, $_csv_matches);
-
-$_csv_data = $_csv_matches[1];
-
-for ($_csv_i = 0; $_csv_i < count($_csv_data); $_csv_i++) {
-$_csv_data[$_csv_i] = preg_replace('/^' . $e . '(.*)' . $e . '$/s', '$1', $_csv_data[$_csv_i]);
-
-$_csv_data[$_csv_i] = str_replace($e . $e, $e, $_csv_data[$_csv_i]);
-
-}
-
-return empty ($_line) ? false : $_csv_data;
-
-}
+    }
 ?>
 
 
