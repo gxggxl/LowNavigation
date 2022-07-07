0cdc96d0c488369bb57a7bf9d4738ea12139c23b
jrotty <bssf@qq.com>
2022-07-07 16:24:54 +0800

    默认启用php版本首页

--- index.php
+++ index.php
@@ -3,6 +3,6 @@ include('config/config.php');//载入主题配置
 if(empty($setting['siteurl'])){
 include('install.php');//进入安装引导   
 }else{
-include('theme/index.php');//进入前台
+include('theme/index1.php');//进入前台
 }
 ?>
